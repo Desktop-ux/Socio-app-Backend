9784e98103c32aa4bbb6cbbf8e5e747d3120ba6e
Ayaan <desktop.ux@gmail.com>
2026-02-08 20:30:14 +0530

    frontend integrated

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser')
 
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin:[ "http://localhost:5173", 'https://socio-gamma.vercel.app'], 
   credentials: true
 }));
 
