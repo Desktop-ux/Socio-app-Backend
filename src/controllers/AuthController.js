8009a5a7834619ffd4efb234aadda7ecacc33c82
Ayaan <desktop.ux@gmail.com>
2026-02-08 22:22:21 +0530

    token error fix

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -51,7 +51,7 @@ const cookies = require("cookie-parser");
    res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000 
   })
   .json({
