658eada8aa56550e55df636122ca5bc32d05df77
Ayaan <desktop.ux@gmail.com>
2026-02-10 02:06:03 +0530

    logout fix

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { signup, login , me } = require("../controllers/AuthController");
+const { signup, login, me } = require("../controllers/AuthController");
 const authMiddleware = require("../midleware/authMiddleware");
 
 const router = express.Router();
@@ -7,9 +7,15 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", (req, res) => {
-  res.clearCookie("token").json({ message: "Logged out" });
-});
-router.get("/me",authMiddleware, me);
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+  });
 
+  res.status(200).json({ message: "Logged out" });
+});
+
+router.get("/me", authMiddleware, me);
 
 module.exports = router;
